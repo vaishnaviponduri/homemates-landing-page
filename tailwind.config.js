bdafa10d768f6ab7790c249cfdd34d97f190ba85
vaishnavi <vaishnaviponduri@gmail.com>
2024-10-08 23:26:55 +0530

    Contact form, footrer

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./src/**/*.{html,js}'], // Paths to your content files
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
